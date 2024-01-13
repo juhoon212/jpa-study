dcb64e2052068666a758a90c6b32a96da1103176
juhoon212 <ws5501@naver.com>
2024-01-13 23:52:48 +0900

    주문,주문 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Orders.java
+++ src/main/java/jpabook/jpashop/domain/Orders.java
@@ -45,12 +45,47 @@ public class Orders {
         this.delivery = delivery;
         delivery.setOrders(this);
     }
-
-
-
+    
     private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // == 생성 메소드 ==
+    public static Orders createOrders(Member member, Delivery delivery, OrderItem...orderItems) {
+        Orders orders = new Orders();
+        orders.setMember(member);
+        orders.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            orders.addOrderItems(orderItem);
+        }
+        orders.setStatus(OrderStatus.ORDER);
+        orders.setOrderDate(LocalDateTime.now());
+        return orders;
+    }
+
+    // == 비즈니스 로직 ==
+    /**
+     * 주문 취소
+     */
+
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 == //
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+        return totalPrice;
+    }
 
 }
